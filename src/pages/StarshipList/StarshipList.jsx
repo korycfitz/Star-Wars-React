6ac5fe27419336d9eced6b77ce089d652c9fe2a0
korycfitz <korycfitz@gmail.com>
2023-05-08 23:45:12 -0400

    code StarshipList page

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,29 +1,32 @@
 import { useState, useEffect } from "react"
-import { getAllStarships } from "../../services/sw-api"
+import { getStarshipList } from "../../services/sw-api"
 import { Link } from 'react-router-dom'
-const MonsterList = () => {
-  const [monsterList, setMonsterList] = useState([])
+
+const StarshipList = () => {
+  const [starshipList, setStarshipList] = useState([])
   
   useEffect(()=> {
-    const fetchMonsterList = async () => {
-      const monsterData = await getMonsterList()
-      setMonsterList(monsterData)
+    const fetchStarshipList = async () => {
+      const starshipData = await getStarshipList()
+      setStarshipList(starshipData)
     }
-    fetchMonsterList()
+    fetchStarshipList()
   }, [])
 
-  if (!monsterList.length) return <h1>Loading scary monsters...</h1>
+  if (!starshipList.length) return <h1>Loading All Starships...</h1>
 
   return (
-    <main className="monster-list">
-      <h1>Monster List (OMG SCARY)</h1>
-      {monsterList.map(monster =>
-        <div key={monster._id} className="link-container">
-          <Link to={`/monsters/${monster._id}`}>{monster.name}</Link>
-        </div>  
-      )}
+    <main className="starship-list">
+      <h1>Starship List</h1>
+      {starshipList.map((starship, idx) => (
+        <Link 
+        to={`/starships/${starship.url.match(/\d+/g)}`} 
+        key={idx}
+        >{starship.name}
+        </Link>
+      ))}
     </main> 
   )
 }
 
-export default MonsterList
+export default StarshipList
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://swapi.dev/api/'
 
-export async function getAllStarships() {
+export async function getStarshipList() {
   //get all the starship resources
   const res = await fetch(`${baseUrl}/starships`)
   return res.json()
@@ -9,10 +9,4 @@ export async function getStarship(starshipId) {
   //get a specific starship resource
   const res = await fetch(`${baseUrl}/starships/${starshipId}`)
   return await res.json()
-}
-
-export async function getPilots(pilotsURLs) {
-  //view the JSON schema for pilot URLs
-  const res = await fetch(`${baseUrl}/api/pilotsURLs`)
-  return res.json()
 }
