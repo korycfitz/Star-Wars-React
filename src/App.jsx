fd1fed07959d3c698ce345fbb076ca326f60e785
korycfitz <korycfitz@gmail.com>
2023-05-09 00:09:39 -0400

    add StarshipList component to NavBar. Add NavBar route and /starships route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import StarshipList from './pages/StarshipList/StarshipList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<StarshipList />}/>
-        <Route path='/starships/:starshipId' element={<StarshipDetails />}/>
+        <Route path='/starships' element={<StarshipList />}/>
+        {/* <Route path='/starships/:starshipId' element={<StarshipDetails />}/> */}
       </Routes>
     </>
   )
