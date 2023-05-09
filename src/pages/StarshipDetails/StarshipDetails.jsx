64449ab7277da19112cfd59e40ac837f6d80d86d
korycfitz <korycfitz@gmail.com>
2023-05-09 01:58:59 -0400

    finish Starship Details page view

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -1,51 +1,34 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { getStarship } from "../../services/api-calls"
+import { getStarship } from "../../services/sw-api"
 import { Link } from "react-router-dom"
 
 const StarshipDetails = () => {
   const [starshipDetails, setStarshipDetails] = useState({})
-  
   const { starshipId } = useParams()
 
   useEffect(() => {
     const fetchDetails = async () => {
-      const StarshipData = await getStarship(StarshipId)
-      setStarshipDetails(StarshipData)
+      const starshipData = await getStarship(starshipId)
+      setStarshipDetails(starshipData)
     }
     fetchDetails()
-  }, [StarshipId])
+  }, [starshipId])
 
-  if (!StarshipDetails.name) return <h1>Loading details...</h1>
+  if (!starshipDetails.name) return <h1>Loading...</h1>
 
   return (
-    <main className="Starship-details">
-      <h1>Starship Deets</h1>
-      {StarshipDetails.image ? 
-        <img 
-          src={`https://www.dnd5eapi.co${StarshipDetails.image}`} 
-          alt={`A (scary) ${StarshipDetails.name}`} 
-          style={{ width: '320px'}}
-        />
-        :  
-        <img 
-          src="http://theoldreader.com/kittens/320/240/" 
-          alt="A cat" 
-        />
-      }
-      <h2>{StarshipDetails.name}</h2>
-      <h3>Size: {StarshipDetails.size}</h3>
-      <h3>Type: {StarshipDetails.type}</h3>
-      <h3>Actions:</h3>
-      {!StarshipDetails.actions?.length &&
-        <h4>This poor Starship has no actions. Womp womp.</h4>
-      }
-      {StarshipDetails.actions?.map(action => (
-        <div key={action.name} className="subsection">
-          <h4>{action.name}</h4>
-          <p>{action.desc}</p>
-        </div>
-      ))}
+    <main className="starship-details">
+      <h1>Starship Details</h1>
+      <h2>Name: {starshipDetails.name}</h2>
+      <h2>Model: {starshipDetails.model}</h2>
+      <Link 
+      to="/starships" 
+      >
+        <button>
+          Return to Starship List
+        </button>
+      </Link>
     </main>
   )
 }
